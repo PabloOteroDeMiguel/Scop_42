1eeea33ee9598889602696706dc883019f0d585b
Pablo Otero <potero91@gmail.com>
2024-02-20 19:04:26 +0100

    install openGL

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Scop_42
+Install OpenGL with brew
+    brew install glfw
+    brew install glew
--- main.c
+++ main.c
@@ -12,14 +12,57 @@
 
 #include "scop.h"
 
-int main(int argc, char** argv){
+// int main(int argc, char** argv){
 
-    if (argc != 2)
-	{
-		ft_putstr_fd("Error\nWrong number of arguments\n", 1);
-		exit(0);
-	}
-    else {
-        ft_putstr_fd(argv[1], 1);
+//     if (argc != 2)
+// 	{
+// 		ft_putstr_fd("Error\nWrong number of arguments\n", 1);
+// 		exit(0);
+// 	}
+//     else {
+//         ft_putstr_fd(argv[1], 1);
+//     }
+// }
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+void Render(void)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+    {
+        glColor3f(1.0,0.0,0.0);
+        glVertex2f(0, .5);
+        glColor3f(0.0,1.0,0.0);
+        glVertex2f(-.5,-.5);
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex2f(.5, -.5);
     }
+    glEnd();
+}
+int main(void) {
+    GLFWwindow* win;
+    if(!glfwInit()){
+        return -1;
+    }
+    win = glfwCreateWindow(640, 480, "OpenGL Base Project", NULL, NULL);
+    if(!win)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    if(!glewInit())
+    {
+        return -1;
+    }
+    glfwMakeContextCurrent(win);
+    while(!glfwWindowShouldClose(win)){
+        Render();
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+    return 0;
 }
